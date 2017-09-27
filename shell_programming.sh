fabfc58f83aabb47945d88013a7fc272bebe4112
roberts.mukans@gmail.com <roberts.mukans@gmail.com>
2017-09-27 11:46:58 +0300

    20170927_11_50

--- shell_programming.sh
+++ shell_programming.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
-NAME="robis ro"
+
+#4.piemers
+echo "tiek pildits skripts: "$0
+#echo $0
+#echo $n
+echo "skriptam nodoto argumentu skaits: "$#
+echo "skriptam nodotie argumenti, veids 1: "$*
+echo "skriptam nodotie argumenti, veids 2: "$@
+#echo $?
+echo "skripta procesa numurs: "$$
+#echo $!
+
+
+#3.piemers
+#NAME="robis ro"
+#echo $NAME
+#unset NAME
 #echo $NAME
 
-
+#2.piemers
+#NAME="robis ro"
 #readonly NAME
 #echo $NAME
 #NAME="ro robis"
 #echo $NAME
 
-
-echo $NAME
-unset NAME
-echo $NAME
+#1.piemers
+#NAME="robis ro"
+#echo $NAME
