f7fb999b62a7a63d83d11ab5cd3c3e3d51194870
roberts.mukans@gmail.com <roberts.mukans@gmail.com>
2017-10-11 11:44:06 +0300

    20171011_11_35

--- shell_operators.sh
+++ shell_operators.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : <<'END'
-#1.piemers
+#1.piemer
 val=`expr 2 + 2`
 echo "Total value : $val"
 
@@ -24,26 +24,40 @@ val=`expr 2 \* 2`
 echo "Total value reizinasana : $val"
 
 #5.piemers
-val=`expr 2 /  2`
-echo "Total value dalisana: $val"
+val=`expr 2 /  2`+echo "Total value dalisana: $val"
 
 #5.piemers
 val=`expr 2 % 2`
 echo "Total value : $val"
 
 END
+: <<'END'
 
 #6.piemers
 a=2
 b=3
 
-if [ $a == $b ]
-then
-echo "a ir vienads ar b"
-fi
+ if [ $a == $b ]
+ then
+ echo "a ir vienads ar b"
+ fi
 
-if [ $a != $b ]
-then
-echo "a nav vienads ar b"
-fi
+ if [ $a != $b ]
+ then
+ echo "a nav vienads ar b"
+ fi
+END
+#7.piemers
 
+if [ $# == 2 ]
+then
+a=$1
+b=$2
+
+
+val=`expr $a + $b`
+echo "$a + $b = $val"
+
+val1=`expr $a - $b`
+echo "$a - $b = $val1"
+fi
