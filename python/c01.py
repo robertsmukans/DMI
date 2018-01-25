21f47676bf1bf2fe01900293ded4bf7094545d94
robertsmukans <31914508+robertsmukans@users.noreply.github.com>
2018-01-25 15:23:54 +0200

    c01.py

---
+++ python/c01.py
@@ -0,0 +1,24 @@
+"""
+Fails : c01 . py
+Autors : Roberts Mukans
+Datums : ...
+Tiek izveidots tukshs fails c01 . txt
+Failu aizpilda ar tekstu . Tekstu modificee . Failu aizver
+"""
+
+fn = 'c01.txt' # definee faila nosaukumu
+
+f = open(fn,"w") # atver failu rakstiit un lasiit
+
+print f.tell() # pateikt atrashanaas vietu failaa
+
+f.write("Python ir asakains\n") # failaa ieraksta simbolu virkni
+
+f.seek (7) # rakstaamgalvu novieto 7. pozicijaa
+
+f.write("- p")
+
+f.close() # Failu aizver ciet !
+
+print "Darbs ar failu pabeigts"
+
